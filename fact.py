2371ec7c2781dc6c1692b73b310109898218ffb5
Dhinen <53181631+Dhinen@users.noreply.github.com>
2019-07-25 00:30:39 -0700

    Update fact.py

--- fact.py
+++ fact.py
@@ -1,9 +1,9 @@
 n=int(input())
 fact=1
 if n<0:
-	print("the factorial doesnot exist for negati=ve num")
+	print("the factorial doesnot exist for negative num")
 elif n==0:
-	print("the factorial 0 is 1")
+	print(1)
 else:
 	for i in range(1,n+1):
 		fact=fact*i
