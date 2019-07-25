af03032b05c3ac23ea97076de8581148b1662d31
Dhinen <53181631+Dhinen@users.noreply.github.com>
2019-07-25 00:28:56 -0700

    Create fact.py

---
+++ fact.py
@@ -0,0 +1,12 @@
+n=int(input())
+fact=1
+if n<0:
+	print("the factorial doesnot exist for negati=ve num")
+elif n==0:
+	print("the factorial 0 is 1")
+else:
+	for i in range(1,n+1):
+		fact=fact*i
+	print(fact)
+	
+	
