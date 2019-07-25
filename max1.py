3fcc11cf46b09ded53f2d6079866023157eaddba
Dhinen <53181631+Dhinen@users.noreply.github.com>
2019-07-25 10:54:28 -0700

    Update max1.py

--- max1.py
+++ max1.py
@@ -5,9 +5,9 @@ for i in range(0,n):
   temp=int(input())
   a.append(temp)
 print(a) 
+max=a[0]
 for j in  range(0,len(a)):
-  max=a[0]
-  if a[j]>max:
+  if max<a[j]:
     max=a[j]
-    print(max)
-    break
+print(max)
+  
