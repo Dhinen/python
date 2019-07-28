7da8e07766692ec19a927a4437d25e712771f5e8
Dhinen <53181631+Dhinen@users.noreply.github.com>
2019-07-28 02:17:19 -0700

    Update and rename alphabet.py to alphabet1.py

--- alphabet.py
+++
@@ -1,6 +0,0 @@
-# your code goes here
-ch=input()
-if (ch>='a' or ch<='z')and(ch>='A' or ch<='Z'):
-	print("is alphabet")
-else:
-	print("is not alphabet")
---
+++ alphabet1.py
@@ -0,0 +1,6 @@
+n=input()
+
+if n.isalpha()==True:
+  print("alphabet")
+else:
+  print("No")  
