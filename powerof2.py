40ff5d02ec61a300745b9d7fb8e431a03a2e2cc5
Dhinen <53181631+Dhinen@users.noreply.github.com>
2019-07-29 07:09:38 -0700

    Update and rename powerof2.py to powerof21.py

--- powerof2.py
+++
@@ -1,5 +0,0 @@
-n=int(input())
-if n%2==0:
-  print("yes")
-else:
-  print("no")  
---
+++ powerof21.py
@@ -0,0 +1,7 @@
+n=int(input())
+if n==1 or n==-1:
+  print("yes") 
+elif n%2==0:
+  print("yes") 
+else:
+  print("no")   
