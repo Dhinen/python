203ba7f87790ce37f16e75ef7b7a59ad4dbad27f
Dhinen <53181631+Dhinen@users.noreply.github.com>
2019-08-01 23:54:54 -0700

    Update klargestnum.py

--- klargestnum.py
+++ klargestnum.py
@@ -1,8 +1,8 @@
 n,k=map(int,input().split())
-m=[str(i) for i in input().split()]
+m=[int(i) for i in input().split()]
 x=sorted(m)
 max=x[0]
 for i in range(1,len(x)):
   if x[i]>max:
-    max=x[i]
+    max=x[i]  
 print(x[i-(k-1)])  
