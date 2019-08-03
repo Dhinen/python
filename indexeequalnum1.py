0944bcb1cd228c0327cc999d500fdf603417eab6
Dhinen <53181631+Dhinen@users.noreply.github.com>
2019-08-02 22:56:16 -0700

    Update indexeequalnum1.py

--- indexeequalnum1.py
+++ indexeequalnum1.py
@@ -1,9 +1,9 @@
 n=int(input())
 m=[int(i) for i in input().split()]
-d=0
+d=-1
 for i in range(0,len(m)):
   if m[i]==i:
     print(m[i],end=" ")
   else:
-    print("-1")  
+    print(d)  
     break 
