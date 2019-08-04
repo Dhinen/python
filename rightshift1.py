2af04dad04874f835cd6c6ca8c27dcc27702ca99
Dhinen <53181631+Dhinen@users.noreply.github.com>
2019-08-03 21:16:30 -0700

    Update rightshift1.py

--- rightshift1.py
+++ rightshift1.py
@@ -2,16 +2,22 @@ n,k=map(int,input().split())
 m=[str(i) for i in input().split()]
 d=[]
 s=0
-for i in range(len(m)-k,len(m)):
+x=""
+if len(m)==1:
+  x=m
+else:  
+ for i in range(len(m)-k,len(m)): 
   if len(m)<k:
-     s=1 
-  else:
     d.append(m[i])
-if s==1:
-  d.append(m[i])
-d=list(dict.fromkeys(d)) 
-for i in range(0,len(m)-k):
+    s=1 
+  else:
+    d.append(m[i])    
+ if s==1:
+  d=dict.fromkeys(d) 
+ for i in range(0,len(m)-k):
   d.append(m[i])     
 for i in d:
-  print(i,end=" ")  
+  print(i,end=" ") 
+for i in x:
+  print(i)   
 
