c9278b8f641395ccbaaa2a60939cc8d69ed4533d
Dhinen <53181631+Dhinen@users.noreply.github.com>
2019-08-03 20:15:28 -0700

    Create rightshift.py

---
+++ rightshift.py
@@ -0,0 +1,10 @@
+n,k=map(int,input().split())
+m=[str(i) for i in input().split()]
+d=[]
+for i in range(len(m)-k,len(m)):
+  d.append(m[i])
+for i in range(0,len(m)-k):
+  d.append(m[i]) 
+for i in d:
+  print(i,end=" ")
+
