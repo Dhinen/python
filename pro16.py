6a6f6f1be987a35b4efde0ea257e1b8f16ac4611
Dhinen <53181631+Dhinen@users.noreply.github.com>
2019-08-09 05:13:02 -0700

    Update pro16.py

--- pro16.py
+++ pro16.py
@@ -1,1 +1,11 @@
-
+s=int(input())
+c=list(map(int,input().split()))
+xy=[1]*s
+for p in range(s):
+    if p==0:
+        if c[p]>c[p+1]:
+            xy[p]=xy[p]+xy[p+1]
+    elif p>0:
+        if c[p]>c[p-1]:
+            xy[p]=xy[p]+xy[p-1]
+print(sum(xy))
