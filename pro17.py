8f58a570945fda0833c201f9a8a4db2e3cb44dc8
Dhinen <53181631+Dhinen@users.noreply.github.com>
2019-08-09 05:37:57 -0700

    Update pro17.py

--- pro17.py
+++ pro17.py
@@ -1,9 +1,9 @@
-x,j=map(int,input().split())
-y=list(map(int,input().split()))[:x]
+x,y=map(int,input().split())
+j=list(map(int,input().split()))[:x]
 count=0
-for k in range(0,len(y)-1):
-  for sec in range(k+1,len(y)-1):
-    if(y[k]+y[sec]==j):
+for k in range(0,len(j)-1):
+  for sec in range(k+1,len(j)-1):
+    if(y[k]+y[sec]==y):
       count+=1  
 if count==1:
   print("yes")
