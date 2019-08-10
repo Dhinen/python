548182050e158e57e090b5fd8d463b792cdd0358
Dhinen <53181631+Dhinen@users.noreply.github.com>
2019-08-10 00:03:18 -0700

    Update hunter7.py

--- hunter7.py
+++ hunter7.py
@@ -1,1 +1,15 @@
+n=int(input())
+s=list(map(int,input().split()))
+d=0
+if n%2!=0:
+  for i in range(0,len(s)):
+    if i%2==0 and s[i]%2==0:
+       break
+    else:
+      s[i]=s[i]
+    print(s[i],end=" ")  
+else:
+    for i in range(0,len(s)):
+      if i%2!=0 and s[i]%2==0:
+       print(s[i],end=" ")
 
