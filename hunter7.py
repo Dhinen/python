0c6a7c3f1c68831474ab2afec12684f004f5eb93
Dhinen <53181631+Dhinen@users.noreply.github.com>
2019-08-10 00:44:36 -0700

    Update hunter7.py

--- hunter7.py
+++ hunter7.py
@@ -1,15 +1,8 @@
 n=int(input())
 s=list(map(int,input().split()))
 d=0
-if n%2!=0:
-  for i in range(0,len(s)):
-    if i%2==0 and s[i]%2==0:
-       break
+for i in range(0,len(s)):
+    if i%2==0 and s[i]%2==0  or i%2!=0 and s[i]%2!=0:
+        continue
     else:
-      s[i]=s[i]
-    print(s[i],end=" ")  
-else:
-    for i in range(0,len(s)):
-      if i%2!=0 and s[i]%2==0:
-       print(s[i],end=" ")
-
+      print(s[i],end=" ")    
