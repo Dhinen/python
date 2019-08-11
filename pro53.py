0380fa8947cbc9f31563bf65dd7c9fe5a2bf9e9f
Dhinen <53181631+Dhinen@users.noreply.github.com>
2019-08-11 07:12:34 -0700

    Update pro53.py

--- pro53.py
+++ pro53.py
@@ -6,9 +6,7 @@ alphabet='abcdefghijklmnopqrstuvwxyz'
 for i in alphabet:
   if i not in d:
     a=1
-  else:
-    a=2  
-if a==2:
-  print("yes")  
+if a==1:
+  print("no")
 else:
-  print("no")  
+  print("yes")      
