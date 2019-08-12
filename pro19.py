9e0f3e8ca866656715386f662114182b46989ecd
Dhinen <53181631+Dhinen@users.noreply.github.com>
2019-08-12 07:57:49 -0700

    Update pro19.py

--- pro19.py
+++ pro19.py
@@ -1,13 +1,9 @@
 n=int(input())
 a=[]
-s=[int(i) for i in input().split()]
-u=[int(i) for i in input().split()]
-v=[int(i) for i in input().split()]
-for i in s:
-  a.append(i)
-for i in u:
-  a.append(i)
-for i in v:
-  a.append(i) 
-b=sorted(a)   
-print(b)  
+for i in range(n):
+ s=list(map(int,input().split()))
+ for j in s:
+  a.append(j)
+b=sorted(a)  
+for i in b:
+  print(i,end=" ")
