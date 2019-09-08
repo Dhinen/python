4dcfc7b8b8db1c294c02410c42056418a417664a
Dhinen <53181631+Dhinen@users.noreply.github.com>
2019-09-08 09:51:19 -0700

    Update reversecaseletter.py

--- reversecaseletter.py
+++ reversecaseletter.py
@@ -2,11 +2,11 @@ N=input()
 d=0
 k=0
 a=[]
-for i in n:
+for i in N:
   if i.islower()==True:
     d=i.upper()
     a.append(d)
   elif i.isupper()==True:
     k=i.lower()
     a.append(k)  
-print(*a)    
+print(''.join(a))    
