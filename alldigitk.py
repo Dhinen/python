11f0043f8be4c6c4b0d9267aa1762e2615d2a7a3
Dhinen <53181631+Dhinen@users.noreply.github.com>
2019-09-19 08:05:04 -0700

    Update alldigitk.py

--- alldigitk.py
+++ alldigitk.py
@@ -1,11 +1,17 @@
 n,m=input().split()
 s=0
+b=[]
+a=[]
+for j in range(0,int(m)+1):
+  b.append(j)          
 for i in range(0,len(n)):
-  if (int(n[i])<int(m))or(int(n[i])==int(m)):
+  a.append(int(n[i]))
+for i in a:
+  if i in b:
     s=1
-  elif int(n[i])>int(m):
-    s=2 
+  elif i not in b:
+    s=2       
 if s==1:
   print("yes") 
 elif s==2:
-  print("no")  
+  print("no") 
