e3a86aac1a5b5f9592d4b0842fd01f1f02475634
Dhinen <53181631+Dhinen@users.noreply.github.com>
2019-09-19 07:16:32 -0700

    Update alldigitk.py

--- alldigitk.py
+++ alldigitk.py
@@ -1,7 +1,7 @@
 n,m=input().split()
 s=0
 for i in range(0,len(n)):
-  if int(n[i])<int(m) or int(n[i])==int(m):
+  if (int(n[i])<int(m))or(int(n[i])==int(m)):
     s=1
   elif int(n[i])>int(m):
     s=2 
