cdf49b8e577bb23bf61689b11a089def814b2c9b
Dhinen <53181631+Dhinen@users.noreply.github.com>
2019-09-21 01:16:59 -0700

    Update folowlarnum1.py

--- folowlarnum1.py
+++ folowlarnum1.py
@@ -5,10 +5,12 @@ k=0
 for i in range(0,len(m)):
   for j in range(i+1,len(m)):
     if m[i]<m[j]:
+      
       t=m[i]
       m[i]=m[j] 
       m[j]=t 
       k=1
+      
 for i in m:
   if k==1:
     print(i,end="") 
