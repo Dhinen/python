e37f1c1a5c92cf6425a42ddeb967d2f47a727626
Dhinen <53181631+Dhinen@users.noreply.github.com>
2019-09-21 01:14:26 -0700

    Update folowlarnum.py

--- folowlarnum.py
+++ folowlarnum.py
@@ -1,16 +1,16 @@
 n=int(input())
 m=[int(i) for i in input().split()]
 s=m[0]
-d=0
+k=0
 for i in range(0,len(m)):
   for j in range(i+1,len(m)):
     if m[i]<m[j]:
       t=m[i]
       m[i]=m[j] 
       m[j]=t 
-      d=1
+      k=1
 for i in m:
-  if d==1:
+  if k==1:
     print(i,end="") 
   elif s==i:
     print(s)
