c34605cacead368ca9eafe5b819a800779fe8b4c
Dhinen <53181631+Dhinen@users.noreply.github.com>
2019-09-21 01:18:42 -0700

    Create asce.py

---
+++ asce.py
@@ -0,0 +1,18 @@
+n=int(input())
+m=[int(i) for i in input().split()]
+s=m[0]
+k=0
+for i in range(0,len(m)):
+  for j in range(i+1,len(m)):
+    if m[i]<m[j]:
+      t=m[i]
+      m[i]=m[j] 
+      m[j]=t 
+      k=1
+for i in m:
+  if k==1:
+    print(i,end="") 
+  elif s==i:
+    print(s)
+    break
+  
